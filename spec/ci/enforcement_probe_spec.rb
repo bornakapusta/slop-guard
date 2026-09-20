1a908537bca871b71c7206a347f2fd3e5bc1f2df
Borna Kapusta <kapusta.borna.s@gmail.com>
2026-09-20 09:14:17 +0200

    Remove failing-test probe after verifying merge enforcement

--- spec/ci/enforcement_probe_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'Temporary merge-enforcement probe' do
-  it 'fails deliberately to verify the required Tests check' do
-    expect(true).to be(false)
-  end
-end
